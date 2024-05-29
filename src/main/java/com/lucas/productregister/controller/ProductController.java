e6d23d69fa0ba34c7549fe79de204f22901c7b71
LukasPio <lucaspio.galvao@gmail.com>
2024-05-28 23:53:51 -0300

    create findAll route to controller

--- src/main/java/com/lucas/productregister/controller/ProductController.java
+++ src/main/java/com/lucas/productregister/controller/ProductController.java
@@ -1,14 +1,30 @@
 package com.lucas.productregister.controller;
 
+import com.lucas.productregister.DTO.ProductResponseDTO;
+import com.lucas.productregister.domain.Product;
+import com.lucas.productregister.repository.ProductRepository;
+import com.lucas.productregister.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
     @GetMapping(path = "/hello")
-    public String hello() {
-        return "Hello World, all are working here.";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World, all are working here.");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> findAll() {
+        return productService.findAll();
     }
 }
