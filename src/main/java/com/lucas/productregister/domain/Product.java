8af0e6e44205318bc329fc73173a30dab8146d61
LukasPio <lucaspio.galvao@gmail.com>
2024-05-28 23:13:58 -0300

    Create model to represent products

---
+++ src/main/java/com/lucas/productregister/domain/Product.java
@@ -0,0 +1,27 @@
+package com.lucas.productregister.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Float price;
+    private Integer quantity;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
