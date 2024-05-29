0861ddf09bbb4d933ff298e99da8a239f1fbd6c3
LukasPio <lucaspio.galvao@gmail.com>
2024-05-28 23:09:49 -0300

    Change lengths of VARCHAR camps

--- src/main/resources/product.sql
+++ src/main/resources/product.sql
@@ -1,19 +1,18 @@
-CREATE DATABASE product;
 \c product;
 
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(254) NOT NULL UNIQUE ,
-    description VARCHAR(499) NOT NULL ,
-    price FLOAT NOT NULL ,
-    quantity INTEGER NOT NULL ,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description VARCHAR(500) NOT NULL,
+    price FLOAT NOT NULL,
+    quantity INTEGER NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO products VALUES (
     DEFAULT,
-    'SmartTV samsumg',
+    'SmartTV Samsung',
     'Experience the next level of home entertainment with Samsung Smart TV. Combining cutting-edge technology, stunning visuals, and seamless connectivity, Samsung Smart TV transforms your living room into an immersive entertainment hub.',
     4699.600,
     5
