2a7614716848cf01f0dd67e17c3e95a63b5cc28f
LukasPio <lucaspio.galvao@gmail.com>
2024-05-31 20:04:41 -0300

    Create logic to save a product

--- src/main/java/com/lucas/productregister/service/ProductService.java
+++ src/main/java/com/lucas/productregister/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.lucas.productregister.service;
 
+import com.lucas.productregister.DTO.ProductRequestDTO;
 import com.lucas.productregister.DTO.ProductResponseDTO;
 import com.lucas.productregister.domain.Product;
 import com.lucas.productregister.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +20,14 @@ public class ProductService {
         List<Product> products = productRepository.findAll();
         return ResponseEntity.ok(products.stream().map(ProductResponseDTO::new).toList());
     }
+
+    public ResponseEntity<String> save(ProductRequestDTO productData) {
+        if (productRepository.existsByName(productData.name())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).
+                    body("Product with name: "+productData.name()+" already exists.");
+        }
+        productRepository.save(new Product(productData));
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product saved.");
+    }
+
 }
