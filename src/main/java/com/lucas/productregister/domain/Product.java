5597795725049d8ebb6f9cf5e19871cf8b2e4254
LukasPio <lucaspio.galvao@gmail.com>
2024-05-31 20:11:18 -0300

    Create setters in runtime

--- src/main/java/com/lucas/productregister/domain/Product.java
+++ src/main/java/com/lucas/productregister/domain/Product.java
@@ -5,12 +5,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "products")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
